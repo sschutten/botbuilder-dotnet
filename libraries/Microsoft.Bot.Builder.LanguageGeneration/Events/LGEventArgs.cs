b2e1c9519fbb4cccaea619ffaa2c46259530d424
hond <hond@microsoft.com>
2020-06-29 15:25:38 +0800

    fix issue

--- libraries/Microsoft.Bot.Builder.LanguageGeneration/Events/LGEventArgs.cs
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration/Events/LGEventArgs.cs
@@ -17,13 +17,5 @@ namespace Microsoft.Bot.Builder.LanguageGeneration
         /// source id of the lg file.
         /// </value>
         public string Source { get; set; }
-
-        /// <summary>
-        /// Gets or sets parse tree context, may include evaluation stack.
-        /// </summary>
-        /// <value>
-        /// LGContext.
-        /// </value>
-        public object Context { get; set; }
     }
 }
