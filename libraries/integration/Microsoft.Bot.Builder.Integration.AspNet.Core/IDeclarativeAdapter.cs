43afa173c991f576ca486496e83e0c2ac1a23969
carlosscastro <ccastro@microsoft.com>
2020-07-14 22:49:17 -0700

    Composer adapters: Create declarative adapter interface placeholder.

---
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/IDeclarativeAdapter.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core
+{
+    /// <summary>
+    /// Interface to express the relationship between an mvc api Controller and a Bot Builder Adapter.
+    /// This interface can be used for Dependency Injection.
+    /// </summary>
+    public interface IDeclarativeAdapter : IBotFrameworkHttpAdapter
+    {
+        /// <summary>
+        /// Gets or sets the adapter name.
+        /// </summary>
+        /// <value>
+        /// The adapter name.
+        /// </value>
+        string Name { get; set; }
+    }
+}
