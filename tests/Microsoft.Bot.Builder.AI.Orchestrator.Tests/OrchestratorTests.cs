266ed59a92058b5ea49fb03d2edb496ab6cd2d8f
Hongyang Du <hond@microsoft.com>
2020-08-05 16:10:14 +0800

    init

---
+++ tests/Microsoft.Bot.Builder.AI.Orchestrator.Tests/OrchestratorTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.AI.Orchestrator.Tests
+{
+    [TestClass]
+    public class OrchestratorTests
+    {
+    }
+}
