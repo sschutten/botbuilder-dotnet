c85a11888a734dd681cee097d68da46a2bd924f1
Hongyang Du <hond@microsoft.com>
2020-08-05 18:43:47 +0800

    fix error

--- tests/Microsoft.Bot.Builder.AI.Orchestrator.Tests/OrchestratorTests.cs
+++ tests/Microsoft.Bot.Builder.AI.Orchestrator.Tests/OrchestratorTests.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder.AI.Orchestrator.Tests
 {
     [TestClass]
     public class OrchestratorTests
     {
+        [TestMethod]
+        public void GG()
+        {
+            var r = "s";
+            Console.WriteLine("f");
+            Assert.AreEqual(r, "s");
+        }
     }
 }
